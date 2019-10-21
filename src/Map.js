6cf8e82197cd1de9fd26dd2ad97a8c120de13121
jason <jasonedlewis@gmail.com>
2019-10-21 11:53:10 -0400

    changed the lat & long of the map

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ReactMapGL, { Marker,Popup } from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 
 
 
@@ -8,20 +8,20 @@ import ReactMapGL, { Marker,Popup } from 'react-map-gl';
 export default function Map(props) {
   const token = "pk.eyJ1IjoiamFzZWdhdGVzIiwiYSI6ImNrMXlmNHRyOTBqbDAzY216bGczOGsxOGsifQ.PoQ-5U96aY_pSlXHv2oEKA"
   const [viewport, setViewport] = useState({
-    latitude: 41.1507,
-    longitude: -73.7542,
+    latitude: 40.7200,
+    longitude: -73.8542,
     width: '100vw',
     height: '100vh',
     zoom: 10,
   });
- 
-  const [selectedSchool, setSelectedSchool ] =useState(null)
+
+  const [selectedSchool, setSelectedSchool] = useState(null)
   console.log(props)
-   const { schools } = props
+  const { schools } = props
 
   return (
     <div>
-      <h1 style={{textAlign:"center", backgroundColor:"#8da9f6"}}>NYC SCHOOLS</h1>
+      <h1 style={{ textAlign: "center", backgroundColor: "#8da9f6" }}>NYC SCHOOLS</h1>
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={token}
@@ -30,32 +30,33 @@ export default function Map(props) {
           setViewport(viewport)
         }}
       >
-       { schools.map(school => (<Marker 
-       latitude={Number(school.latitude)} 
-       longitude={Number(school.longitude)}
-       key={school.system_code}
-       >
-           <div ><button onClick={(e)=> {
-               e.preventDefault(); 
+        {schools.map(school => (<Marker
+          latitude={Number(school.latitude)}
+          longitude={Number(school.longitude)}
+          key={school.system_code}
+        >
+          <div ><button onClick={(e) => {
+            e.preventDefault();
             //    console.log(school)
-               setSelectedSchool(school)}
-            }
-           
-                role="img" aria-label="school-bus"
-                 >🚌</button></div>
-       </Marker>))
-       }
-       {selectedSchool ? (
-           <Popup latitude={Number(selectedSchool.latitude)} 
-           longitude={Number(selectedSchool.longitude)}
-           onClose={()=> setSelectedSchool(null)}
-           >
+            setSelectedSchool(school)
+          }
+          }
+
+            role="img" aria-label="school-bus"
+          >🚌</button></div>
+        </Marker>))
+        }
+        {selectedSchool ? (
+          <Popup latitude={Number(selectedSchool.latitude)}
+            longitude={Number(selectedSchool.longitude)}
+            onClose={() => setSelectedSchool(null)}
+          >
 
             <div><strong>School Name:</strong> {selectedSchool.location_name}</div>
             <p><strong>Level:</strong> {selectedSchool.location_category_description}</p>
             <p><strong>Location:</strong> {selectedSchool.primary_address_line_1}</p>
-           </Popup>
-       ) : null}
+          </Popup>
+        ) : null}
       </ReactMapGL>
     </div>
   );
